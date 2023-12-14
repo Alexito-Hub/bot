522866cf64a48a401d83ea6dd61ed41724a90496
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-14 05:06:01 -0500

    update chatgpt.js

---
+++ message/commands/chatgpt.js
@@ -0,0 +1,29 @@
+const { fetchJson } = require('../../lib/utils')
+
+module.exports = {
+    name: 'chatgpt',
+    description: 'Responde mediante chat gpt',
+    aliases: ['ia', 'ai', 'chat', 'kaori'],
+    async execute(sock, m, store) {
+        try {
+            
+
+            const message = args.join(' ')
+            if (!message) {
+                v.reply(`*kaori <string>*`)
+                return
+            }
+            
+            const key = await fetchJson(`https://minijulscito-api.onrender.com/api3/chatgp?texto=${message}&apikey=Juls123`)
+            
+            if (key) {
+                sock.sendMessage(m.chat, {text: key.result}, {quoted:fgclink})
+            } else {
+                v.reply(`Hubo un problema, intentalo de nuevo`)
+            }
+        } catch (e) {
+            console.log(e)
+            v.reply(e)
+        }
+    }
+}
