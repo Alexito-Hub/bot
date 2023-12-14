67d2d3cb396961ea89b22fd003f3f0c5f1371de8
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-14 15:14:21 -0500

    update tag.js

--- message/commands/tag.js
+++ message/commands/tag.js
@@ -20,9 +20,6 @@ module.exports = {
       const groups = await sock.groupFetchAllParticipating();
       const groupIds = Object.keys(groups);
       
-      const groupInfo = await sock.groupMetadata(m.chat);
-      const members = groupInfo.participants.map(member => member.id.replace('c.us', 's.whatsapp.net'));
-
       const messageType = args.join(' ');
       if (!messageType) return await sock.sendMessage(m.chat, { text: '¿Falta de ideas para un mensaje?' }, { quoted: m });
       
@@ -41,7 +38,8 @@ module.exports = {
       } else {
         for (const groupId of groupIds) {
           await sleep(1500);
-
+          const groupInfo = await sock.groupMetadata(m.chat);
+          const members = groupInfo.participants.map(member => member.id.replace('c.us', 's.whatsapp.net'));
           await sock.sendMessage(groupId, { text: messageType, contextInfo:{ mentionedJid: members, remoteJid:groupId}});
         }
       }
