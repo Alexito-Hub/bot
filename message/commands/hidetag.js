19f67667e526125eb604d6b127cf39571032669f
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-14 14:42:28 -0500

    update hidetag.js

--- message/commands/hidetag.js
+++ message/commands/hidetag.js
@@ -1,8 +1,9 @@
+
 module.exports = {
     name: 'hidetag',
     description: 'Menciona a todos los miembros del grupo con un mensaje oculto',
     
-    async execute(sock, m, args, groupMetadata) {
+    async execute(sock, m, groupMetadata, args) {
         try {
             if (!m.isGroup) {
                 sock.sendMessage(m.chat, { text: 'Este comando solo se puede usar en grupos.' }, { quoted: m });
@@ -10,16 +11,11 @@ module.exports = {
             }
 
             const groupInfo = await groupMetadata(m.chat);
-            const members = groupInfo.participants.map(adm => adm.id.replace('c.us', 's.whatsapp.net'));
+            const members = groupInfo.participants.map(member => member.id.replace('c.us', 's.whatsapp.net'));
 
             const message = args.join(' ');
 
-            const options = {
-                text: message,
-                contextInfo: { mentionedJid: members },
-            };
-
-            await sendMessage(m.chat, options, {quoted:m});
+            await sendMessage(m.chat, { text: message, contextInfo:{mentionedJid: members}}, {quoted:m});
         } catch (error) {
             console.error('Error al ejecutar el comando hidetag:', error);
             sock.sendMessage(m.chat, { text: 'Error al ejecutar el comando hidetag.' }, { quoted: m });
