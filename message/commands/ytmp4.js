6f8788c8fcfee51adee30349dbefa20a13bb4ecc
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-14 20:22:48 -0500

    update ytmp4.js

---
+++ message/commands/ytmp4.js
@@ -0,0 +1,43 @@
+const ytdl = require('ytdl-core');
+
+module.exports = {
+    name: 'ytmp4',
+    description: 'Descargar un video de YouTube en formato mp4.',
+    aliases: ['mp4'],
+
+    async execute(sock, m, args) {
+        try {
+            if (!args[0]) {
+                return sock.sendMessage(m.chat, 'Por favor, proporciona una URL de YouTube.');
+            }
+
+            const videoUrl = args[0];
+            const info = await ytdl.getInfo(videoUrl);
+
+            const videoInfo = {
+                title: info.videoDetails.title,
+                author: info.videoDetails.author.name,
+                lengthSeconds: info.videoDetails.lengthSeconds,
+                views: info.videoDetails.viewCount,
+                url: videoUrl,
+            };
+
+            const videoStream = ytdl(videoUrl, { quality: 'highestvideo' });
+
+            await sock.sendMessage(m.chat, { video: videoStream, mimetype: 'video/mp4', caption: formatVideoInfo(videoInfo) }, { quoted: m });
+        } catch (error) {
+            console.error('Error en el comando ytmp4:', error);
+            sock.sendMessage(m.chat, 'Se produjo un error al ejecutar el comando ytmp4.');
+        }
+    },
+};
+
+function formatVideoInfo(videoInfo) {
+    return `*${videoInfo.title}*\n\nAutor: ${videoInfo.author}\nDuración: ${formatDuration(videoInfo.lengthSeconds)}\nVistas: ${videoInfo.views}\nURL: ${videoInfo.url}`;
+}
+
+function formatDuration(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+}
