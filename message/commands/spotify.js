786e9ee36413c1e94f4eca1565570b97b2b9abb8
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-27 00:35:40 -0500

    up

---
+++ message/commands/spotify.js
@@ -0,0 +1,37 @@
+const { fetchJson } = require('../../lib/utils');
+
+module.exports = {
+    name: 'spotify',
+    description: 'Descarga musicas de Spotify',
+    async execute(sock, m, store, args) {
+        try {
+            if (!args[0]) {
+                await sock.sendMessage(m.chat, { text: '*spotify <string>*' }, { quoted: m });
+                return;
+            }
+            await sock.sendMessage(m.chat, { react: { text: '🕛', key: m.key } });
+            const text = args.join(' ');
+            const searchSpotify = await fetchJson(`http://sabapi.tech:8090/api/spotifysearch?query=${text}&apikey=MrRootsFree`)
+            const imgSpotify = searchSpotify.resultado.tracksArray[0]
+            const spotify = await fetchJson(`http://sabapi.tech:8090/api/dl/spotify2?query=${text}&apikey=MrRootsFree`);
+            if (spotify && spotify.resultado && spotify.resultado.dl_link) {
+                sock.sendMessage(m.chat, { image:{ 
+                    url: imgSpotify.thumb, mimetype: 'image/jpeg'},
+                    caption: ` -- SPOTIFY -- 
+Nombre: ${spotify.resultado.trackName}
+Albun: ${spotify.resultado.albumName}
+Artista: ${spotify.resultado.artists[0].name}
+Duración: ${spotify.resultado.duration}
+Popularidad: ${spotify.resultado.popularity}
+Fecha: ${spotify.resultado.release_data}`
+                }, {quoted: m})
+                sock.sendMessage(m.chat, { audio: { url: spotify.resultado.dl_link }, mimetype: 'audio/mpeg'}, {quoted: m});
+            }
+
+            await sock.sendMessage(m.chat, { react: { text: '✅', key: m.key } });
+        } catch (e) {
+            await sock.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
+        }
+
+    }
+}
