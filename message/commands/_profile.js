c43a6de48711323421a0cfc4e3ec28411460b5d3
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-01 12:20:53 -0500

    up

--- message/commands/_profile.js
+++ message/commands/_profile.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
-const { fetchJson, getBuffer } = require('../../lib/utils');
+const { fetchJson } = require('../../lib/utils');
 
 module.exports = {
     name: "Perfil",
-    description: "Obten información de usuario",
+    description: "Obtener información de usuario",
     aliases: ["profile", "perfil"],
     
     async execute(sock, m, args, store) {
@@ -11,40 +11,40 @@ module.exports = {
             let user;
             
             if (m.sender) {
-                user = m.sender.split(`@`)[0]
+                user = m.sender.split('@')[0];
             } else if (args.length > 0) {
                 user = args[0].replace('@', '').replace(/\s/g, '').split('@')[0];
             } else if (m.quoted) {
-                user = m.quoted.sender.split(`@`)[0]
+                user = m.quoted.sender.split('@')[0];
             } else {
                 sock.sendMessage(m.chat, { text: '*Profile <@usuario>*' }, { quoted: m });
                 return;
             }
             
-            const profile = await fetchJson(`https://api-zio.replit.app/api/users/${user}?key=ZioAPI`)
-            const dataUser = profile.result
+            const profile = await fetchJson(`https://api-zio.replit.app/api/users/${user}?key=ZioAPI`);
+            const dataUser = profile.result;
             
             if (profile.status === 404) {
                 sock.sendMessage(m.chat, { text: 'Usuario no registrado' }, { quoted: m });
                 return;
             }
+
+            let ppuser;
             
             try {
-                ppuser = await sock.profilePictureUrl(`${sender.split('@')[0]}@c.us`, 'image')
-                
+                ppuser = await sock.profilePictureUrl(`${user}@c.us`, 'image');
             } catch {
-                ppuser = 'https://telegra.ph/file/8615e70dd92328db2395b.mp4'
-                
+                ppuser = 'https://telegra.ph/file/8615e70dd92328db2395b.mp4';
             }
-            buffer = await sock.getBuffer(ppuser)
-            
+
+            const buffer = await sock.getBuffer(ppuser);
+
             sock.sendMessage(m.chat, { 
                 image: {url: buffer},
                 mimetype: 'image/jpeg',
                 caption: `ㅤㅤ *⋯⋯ PROFILE ⋯⋯*
 
  *➭ Numero:* ${user}
- 
  *➭ Nombre:* ${dataUser.name}
  *➭ Edad:* ${dataUser.age}
  *➭ Genero:* ${dataUser.gender}
@@ -54,11 +54,11 @@ module.exports = {
  *➭ Advertencias* ${dataUser.warning}
  *➭ Limites:* ${dataUser.limit}
  
- *➭ Foto de Perfil:*
-`
-            }, { quoted: m })
+ *➭ Foto de Perfil:*`
+            }, { quoted: m });
         } catch (e) {
-            throw e
+            console.error('Error:', e);
+            sock.sendMessage(m.chat, { text: 'Error al procesar la solicitud.' }, { quoted: m });
         }
     }
-}
+};
