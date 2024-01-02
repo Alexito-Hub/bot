8da7a7c8eecab331d193084d5e15b55f8a5d4bca
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-01 23:57:53 -0500

    update index.js

--- index.js
+++ index.js
@@ -48,49 +48,54 @@ const start = async () => {
 
 		sock.ev.on('creds.update', saveCreds);
 		
-		sock.ev.on('group-participants.update', async (update) => {
-            console.log('group-participants.update event triggered');
-            const groupId = update.id;
-            const participants = update.participants;
-            const action = update.action;
-    
-            for (const participant of participants) {
-                console.log(`participant update: ${participant}, action: ${action}`);
-                const user = participant.split('@')[0];
-                if (action === 'add') {
-                    sock.sendMessage(groupId, { 
-                        text:`¡Bienvenido/a @${user} al grupo NaN! ¡Espero que disfrutes tu estancia y compartas momentos geniales!`,
-                        contextInfo: {
-                            mentionedJid: [participant],
-                            externalAdReply: {
-                                title: `ᴍᴏᴄʜɪ • ᴛᴀᴋᴜ ᴍᴇᴅɪᴀ`,
-                                body: `ugu`,
-                                showAdAttribution: true,
-                                renderLargerThumbnail: true, 
-                                mediaType: 1, 
-                                thumbnailUrl: 'https://imgmedia.larepublica.pe/640x371/larepublica/original/2022/06/30/62be22d15330dd1f2a2f91c0.webp'
-                                }
-                            }
-                    })
-    
-                } else if (action === 'remove') {
-                    sock.sendMessage(groupId, {
-                        text:`Lamentamos ver partir a @${user}. Siempre serás bienvenido/a de regreso si decides volver. ¡Hasta pronto y te deseamos lo mejor!`,
-                        contextInfo: {
-                            mentionedJid: [participant],
-                            externalAdReply: {
-                                title: `ᴍᴏᴄʜɪ • ᴛᴀᴋᴜ ᴍᴇᴅɪᴀ`,
-                                body: `Ugu`,
-                                showAdAttribution: true,
-                                renderLargerThumbnail: true, 
-                                mediaType: 1, 
-                                thumbnailUrl: 'https://imgmedia.larepublica.pe/640x371/larepublica/original/2022/06/30/62be22d15330dd1f2a2f91c0.webp'
-                                }
-                            }
-                    })
+sock.ev.on('group-participants.update', async (update) => {
+    console.log('group-participants.update event triggered');
+    const groupId = update.id;
+    const participants = update.participants;
+    const action = update.action;
+    const inv = update.inviter;
+
+    const groupName = await sock.getName(groupId);
+
+    for (const participant of participants) {
+        console.log(`participant update: ${participant}, action: ${action}`);
+        const user = participant.split('@')[0];
+        const invite = inv ? await sock.getName(inv) : 'desconocido';
+
+        if (action === 'add') {
+            sock.sendMessage(groupId, { 
+                text: `¡Bienvenido/a @${user} al grupo ${groupName}! Añadido por @${invite}. ¡Espero que disfrutes tu estancia y compartas momentos geniales!`,
+                contextInfo: {
+                    mentionedJid: [participant],
+                    externalAdReply: {
+                        title: `ᴍᴏᴄʜɪ • ᴛᴀᴋᴜ ᴍᴇᴅɪᴀ`,
+                        body: `ugu`,
+                        showAdAttribution: true,
+                        renderLargerThumbnail: true, 
+                        mediaType: 1, 
+                        thumbnailUrl: 'https://imgmedia.larepublica.pe/640x371/larepublica/original/2022/06/30/62be22d15330dd1f2a2f91c0.webp'
+                    }
                 }
-            }
-        });
+            });
+        } else if (action === 'remove') {
+            sock.sendMessage(groupId, {
+                text: `Lamentamos ver partir a @${user}. Eliminado por @${invite} del grupo ${groupName}. Siempre serás bienvenido/a de regreso si decides volver. ¡Hasta pronto y te deseamos lo mejor!`,
+                contextInfo: {
+                    mentionedJid: [participant],
+                    externalAdReply: {
+                        title: `ᴍᴏᴄʜɪ • ᴛᴀᴋᴜ ᴍᴇᴅɪᴀ`,
+                        body: `Ugu`,
+                        showAdAttribution: true,
+                        renderLargerThumbnail: true, 
+                        mediaType: 1, 
+                        thumbnailUrl: 'https://imgmedia.larepublica.pe/640x371/larepublica/original/2022/06/30/62be22d15330dd1f2a2f91c0.webp'
+                    }
+                }
+            });
+        }
+    }
+});
+
 		sock.ev.on('messages.upsert', messages => {
 			messages = messages.messages[0];
 			if (!messages) return;
