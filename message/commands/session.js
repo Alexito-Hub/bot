96a8850a38a0d4543c1557e3a71d2285c2661950
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-06 15:10:37 -0500

    update session.js

--- message/commands/session.js
+++ message/commands/session.js
@@ -14,18 +14,14 @@ module.exports = {
 
             const searchText = args.join(' ');
 
-            // Supongamos que aquí está la lógica para buscar en YouTube y obtener los resultados
             const searchResults = await fetchJson(`https://iam-zio.replit.app/api/ytdl-search?key=zio&q=${searchText}`);
 
             if (!searchResults || !searchResults.result) {
                 await sock.sendMessage(m.chat, { text: 'No se encontraron resultados.' }, { quoted: m });
                 return;
             }
-
-            // Obtener el primer resultado
             const firstResult = searchResults.result[0];
 
-            // Construir el mensaje con la información del video y opciones de descarga
             const message = `
                 *⋯⋯ PLAY ⋯⋯*
                 ∘ *Nombre:* ${firstResult.title}
@@ -37,12 +33,9 @@ module.exports = {
                 2. Descargar en Audio
             `;
 
-            // Enviar el mensaje con las opciones y obtener el ID del mensaje enviado
             const optionsMessage = await sock.sendMessage(m.chat, { text: message }, { quoted: m });
             
-
-            // Esperar la respuesta del usuario con un tiempo de espera de 2 minutos
-            const response = await sock.waitForMessage(m.chat, 120000);
+            const response = await sock.waitForMessage(optionsMessage, 120000);
 
             if (!response || !response.body) {
                 await sock.sendMessage(m.chat, { text: 'Tiempo de espera agotado o respuesta no válida. Inténtalo de nuevo.' }, { quoted: m });
@@ -53,10 +46,8 @@ module.exports = {
             
             if (choice === '1') {
                 await sock.sendMessage(m.chat, { text: `Descargando el video: ${firstResult.url}` }, { quoted: m });
-                // Lógica para descargar el video
             } else if (choice === '2') {
                 await sock.sendMessage(m.chat, { text: `Descargando el audio: ${firstResult.url}` }, { quoted: m });
-                // Lógica para descargar el audio
             } else {
                 await sock.sendMessage(m.chat, { text: 'Opción no válida.' }, { quoted: m });
             }
