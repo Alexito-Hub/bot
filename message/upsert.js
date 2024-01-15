60d55917636340b9391a4f58b49fe3cc1aa269cb
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-15 13:36:02 -0500

    update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -67,6 +67,7 @@ module.exports = async(sock, m, store) => {
         const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
         const hasCommandPrefix = prefixes.some(prefix => m.body.toLowerCase().startsWith(prefix.toLowerCase()));
+        const commandBody = hasCommandPrefix && m.body ? m.body.slice(prefixes.find(prefix => m.body.toLowerCase().startsWith(prefix.toLowerCase())).length).trim() : m.body ? m.body.trim() : '';
         const [commandName, ...commandArgs] = commandBody.split(/ +/);
         
         const commandInfo = getCommandInfo(commandName.toLowerCase());
